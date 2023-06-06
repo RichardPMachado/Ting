f16ad36de1da4d81da5bbf2ba6b0c7d06fdc6643
RichardPMachado <richardmachado2211@gmail.com>
2023-06-06 16:43:25 -0300

    feat[requisito 2]: 3/3

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,17 @@
-def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+import sys
+
+
+def txt_importer(file_path):
+    try:
+        if not file_path.endswith(".txt"):
+            raise ValueError("Formato inválido")
+
+        with open(file_path, "r") as file:
+            lines = file.read().split("\n")
+            return lines
+
+    except FileNotFoundError:
+        print(f"Arquivo {file_path} não encontrado", file=sys.stderr)
+
+    except ValueError as e:
+        print(str(e), file=sys.stderr)
