0e52bcda72cf0d73bdac7d5c762703c802862aac
RichardPMachado <richardmachado2211@gmail.com>
2023-06-07 02:00:52 -0300

    feat[requisito 7]: 2/2

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,23 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    results = list()
+
+    for file in range(len(instance)):
+        file = instance.search(file)
+
+        file_obj = {
+                "palavra": word,
+                "arquivo": file["nome_do_arquivo"],
+                "ocorrencias": list(),
+            }
+
+        for index, line in enumerate(file["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                file_obj["ocorrencias"].append({"linha": index + 1})
+
+        if file_obj["ocorrencias"]:
+            results.append(file_obj)
+
+    return results
 
 
 def search_by_word(word, instance):
